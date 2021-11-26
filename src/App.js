009b6c8c63ac0600cfef1f527777a7e2b31e0ad1
Tariq Jamal <tariqjamal4267324@gmail.com>
2021-11-26 22:30:39 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './Components/Login/Login';
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
